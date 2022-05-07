cb1759badf17280e9e03f475d917cf8bd79de7ce
Tajinder S <63464204+Tejinder15@users.noreply.github.com>
2022-05-07 11:58:49 +0530

    refactor: authprovider and refactor code

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 import React from "react";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
 import {BrowserRouter} from "react-router-dom";
+import { AuthProvider } from "./Context/AuthContext/auth-context";
+import {createRoot} from "react-dom/client";
 
 
 // Call make Server
 makeServer();
-
-ReactDOM.render(
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+root.render(
   <React.StrictMode>
     <BrowserRouter>
+    <AuthProvider>
     <App />
+    </AuthProvider>
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
