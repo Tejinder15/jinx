901fc288efef327f390217e4ccd2e61df3717b12
Tajinder S <63464204+Tejinder15@users.noreply.github.com>
2022-05-09 13:50:53 +0530

    new: export components

---
+++ src/Components/index.js
@@ -0,0 +1,7 @@
+export {BookPost} from "./BookPost/BookPost";
+export {FollowComp} from "./FollowComp/FollowComp";
+export {Footer} from "./Footer/Footer";
+export {Header} from "./Header/Header";
+export {LeftPanel} from "./LeftPanel/LeftPanel";
+export {Posts} from "./Posts/Posts";
+export {RightPanel} from "./RightPanel/RightPanel";
