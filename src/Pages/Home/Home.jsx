20d523365a1bf9dc3e7f3e3d9140a55adb946312
Tajinder S <63464204+Tejinder15@users.noreply.github.com>
2022-05-10 13:53:32 +0530

    added postid

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -19,6 +19,7 @@ const Home = () => {
                   key={item.id}
                   profile={item.profile}
                   username={item.username}
+                  postid={item._id}
                 />
               ))
             ) : (
