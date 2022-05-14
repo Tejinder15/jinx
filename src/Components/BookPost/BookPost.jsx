2b7823f776ecf277bf1cd1f7a6cba19846ee9132
Tajinder S <63464204+Tejinder15@users.noreply.github.com>
2022-05-15 00:21:36 +0530

    added Previous Handlers

--- src/Components/BookPost/BookPost.jsx
+++ src/Components/BookPost/BookPost.jsx
@@ -1,15 +1,31 @@
-import { MdBookmark } from "react-icons/md";
+import { MdFavorite, MdBookmark, MdTextsms, MdShare } from "react-icons/md";
+import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../Context/AuthContext/auth-context";
 import { useBookmark } from "../../Context/BookContext/bookmark-context";
+import { usePost } from "../../Context/PostContext/post-context";
 import { delFromBookmark } from "../../Utils/delfrombookmark";
-const BookPost = ({ content, profile, username, postid }) => {
+import { likePost } from "../../Utils/likepost";
+import { dislikePost } from "../../Utils/dislikepost";
+const BookPost = ({ content, profile, username, postid, likedBy }) => {
   const {
-    authState: { token },
+    authState: { user, token },
   } = useAuth();
   const { bookmarkDispatch } = useBookmark();
 
+  const { postDispatch } = usePost();
+
+  const navigate = useNavigate();
+
   const delHandler = (postid) => {
     delFromBookmark(postid, token, bookmarkDispatch);
+  };
+
+  const likeHandler = async (postid) => {
+    likePost(postid, token, postDispatch);
+  };
+
+  const dislikeHandler = async (postid) => {
+    dislikePost(postid, token, postDispatch);
   };
   return (
     <div className="mt-5 bg-white rounded-lg px-1 border border-gray-400 mx-auto shadow-sm">
@@ -29,6 +45,30 @@ const BookPost = ({ content, profile, username, postid }) => {
         <p className="py-2 px-4">{content}</p>
       </div>
       <div className="flex items-center p-2 px-4 justify-between">
+        {likedBy.some((item) => item.username === user.username) ? (
+          <button
+            className="text-2xl mx-1 text-red-500"
+            onClick={() => dislikeHandler(postid)}
+          >
+            <MdFavorite />
+          </button>
+        ) : (
+          <button
+            className="text-2xl mx-1 text-slate-400"
+            onClick={() => likeHandler(postid)}
+          >
+            <MdFavorite />
+          </button>
+        )}
+        <button
+          className="text-2xl mx-1 ml-3 text-slate-400"
+          onClick={() => navigate(`/post/${postid}`)}
+        >
+          <MdTextsms />
+        </button>
+        <button className="text-2xl mx-1 ml-3 text-slate-400">
+          <MdShare />
+        </button>
         <button className="text-2xl text-orange-500 ml-auto">
           <MdBookmark onClick={() => delHandler(postid)} />
         </button>
