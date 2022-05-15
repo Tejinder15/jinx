fe915555893f9a635f062f875642b72602f69ced
Tajinder S <63464204+Tejinder15@users.noreply.github.com>
2022-05-15 17:35:34 +0530

    refactor: added delete option

--- src/Pages/Post/Post.jsx
+++ src/Pages/Post/Post.jsx
@@ -1,17 +1,31 @@
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { usePost } from "../../Context/PostContext/post-context";
 import { Header } from "../../Components";
 import { Posts } from "../../Components";
+import { MdDelete } from "react-icons/md";
+import { useAuth } from "../../Context/AuthContext/auth-context";
+import { addComment } from "../../Utils/addcomment";
+import { deleteComment } from "../../Utils/deletecomment";
 
 const Post = () => {
   const { postId } = useParams();
   const {
     postState: { posts },
+    postDispatch,
   } = usePost();
+  const navigate = useNavigate();
+
+  const {
+    authState: {
+      user: { username },
+      token,
+    },
+  } = useAuth();
 
   const [gotPost, setGotPost] = useState(null);
+  const [comment, setComment] = useState("");
 
   const getThatPost = async () => {
     try {
@@ -26,24 +40,31 @@ const Post = () => {
     }
   };
 
-  const getComments = async () => {
-    try {
-      const response = await axios.get(`/api/comments/${postId}`);
-      console.log(response);
-    } catch (error) {
-      console.error(error);
+  const addCommentHandler = (postId) => {
+    if (token) {
+      addComment(postId, token, comment, postDispatch);
+      setComment("");
+    } else {
+      navigate("/login");
+    }
+  };
+
+  const deleteCommentHandler = (commentId) => {
+    if (token) {
+      deleteComment(postId, commentId, token, postDispatch);
+    } else {
+      navigate("/login");
     }
   };
 
   useEffect(() => {
     getThatPost();
-    getComments();
-    console.log("inside effect");
+    // eslint-disable-next-line
   }, [posts]);
   return (
     <div>
       <Header />
-      <section className="max-w-xl mx-auto pb-16 bg-white p-3">
+      <section className="max-w-xl mx-auto pb-16 bg-white p-3 rounded-md">
         {gotPost ? (
           <Posts
             content={gotPost.content}
@@ -54,18 +75,57 @@ const Post = () => {
             likedBy={gotPost.likes.likedBy}
           />
         ) : null}
-        <div className="border flex justify-between items-center">
+        <div className="border flex justify-between items-center rounded-md">
           <input
             type="text"
             name="comment"
             id="comment"
-            autoFocus
             placeholder="comment"
             className="p-2 rounded-md outline-none bg-slate-200 w-11/12"
+            autoFocus
+            onChange={(e) => setComment(e.target.value)}
+            value={comment}
           />
-          <button className="bg-orange-500 p-2 rounded text-white">
+          <button
+            className="bg-orange-500 p-2 rounded text-white"
+            onClick={() => addCommentHandler(postId)}
+          >
             Comment
           </button>
+        </div>
+
+        <div className="mt-3">
+          {gotPost ? (
+            gotPost.comments.length > 0 ? (
+              gotPost.comments.map((item) => (
+                <div
+                  className="max-w-xl shadow-md border mb-5 bg-white rounded-lg px-1"
+                  key={item._id}
+                >
+                  <div className="flex items-center px-3 py-2">
+                    <div className="pl-2">
+                      <p className="font-semibold text-slate-700 mb-auto">
+                        {item.username}
+                      </p>
+                    </div>
+                    {item.username === username && (
+                      <button
+                        className="ml-auto text-2xl text-slate-400 hover:text-red-500"
+                        onClick={() => deleteCommentHandler(item._id)}
+                      >
+                        <MdDelete />
+                      </button>
+                    )}
+                  </div>
+                  <div className="px-1">
+                    <p className="py-2 px-4">{item.text}</p>
+                  </div>
+                </div>
+              ))
+            ) : (
+              <h2>No Comments on this Post.</h2>
+            )
+          ) : null}
         </div>
       </section>
     </div>
