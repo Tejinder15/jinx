1cb0254cd3292c708a2956290fe2b7c814428fce
Tajinder S <63464204+Tejinder15@users.noreply.github.com>
2022-05-21 13:03:38 +0530

    refactor: added provider

--- src/index.js
+++ src/index.js
@@ -3,10 +3,9 @@ import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
 import {BrowserRouter} from "react-router-dom";
-import { AuthProvider } from "./Context/AuthContext/auth-context";
 import {createRoot} from "react-dom/client";
-import { PostProvider } from "./Context/PostContext/post-context";
-import { BookMarkProvider } from "./Context/BookContext/bookmark-context";
+import {store} from "./redux/app/store";
+import { Provider } from "react-redux";
 
 
 // Call make Server
@@ -16,13 +15,9 @@ const root = createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <AuthProvider>
-    <PostProvider>
-    <BookMarkProvider>
-    <App />
-    </BookMarkProvider>
-    </PostProvider>
-    </AuthProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
