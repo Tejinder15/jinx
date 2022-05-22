d115a54ba9db0725423efa569b6885f4da6ad694
Tajinder S <63464204+Tejinder15@users.noreply.github.com>
2022-05-22 21:31:39 +0530

    feat: bookmark slice and thunk

--- src/redux/app/store.js
+++ src/redux/app/store.js
@@ -1,10 +1,12 @@
 import {configureStore} from "@reduxjs/toolkit";
+import bookReducer  from "../features/book/bookSlice";
 import authReducer from '../features/auth/authSlice';
 import postReducer from '../features/post/postSlice';
 
 export const store = configureStore({
     reducer:{
         auth: authReducer,
-        posts: postReducer
+        posts: postReducer,
+        bookmarks: bookReducer
     }
 });
