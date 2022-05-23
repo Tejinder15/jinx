a550e19b1804ae0e5c983a447468d91c052e9aaa
Tajinder S <63464204+Tejinder15@users.noreply.github.com>
2022-05-23 09:14:34 +0530

    refactor: book thunk

--- src/redux/thunks/bookThunk.js
+++ src/redux/thunks/bookThunk.js
@@ -1,10 +1,9 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const getAllBookmark = createAsyncThunk('bookmark/getBookmark',async({token})=>{
+const getAllBookmark = createAsyncThunk('bookmark/getAllBookmark',async({token})=>{
     try {
         const response = await axios.get('/api/users/bookmark',{headers:{authorization:token}});
-        console.log(response);
         return response.data.bookmarks;
     } catch (error) {
         console.error(error);
@@ -13,8 +12,7 @@ const getAllBookmark = createAsyncThunk('bookmark/getBookmark',async({token})=>{
 
 const addBookmark = createAsyncThunk('bookmark/addBookmark',async({postid,token})=>{
     try {
-        const response = await axios.post(`/api/users/bookmark/${postid}`,{headers: {authorization:token}});
-        console.log(response);
+        const response = await axios.post(`/api/users/bookmark/${postid}`,{},{headers: {authorization:token}});
         return response.data.bookmarks;
     } catch (error) {
         console.error(error);
@@ -23,8 +21,7 @@ const addBookmark = createAsyncThunk('bookmark/addBookmark',async({postid,token}
 
 const delBookmark = createAsyncThunk('bookmark/delBookmark',async({postid,token})=>{
     try {
-        const response = await axios.post(`/api/users/bookmark/${postid}`,{headers:{authorization:token}});
-        console.log(response);
+        const response = await axios.post(`/api/users/remove-bookmark/${postid}`,{},{headers:{authorization:token}});
         return response.data.bookmarks;
     } catch (error) {
         console.error(error);
