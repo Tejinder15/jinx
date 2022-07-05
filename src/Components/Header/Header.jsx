c105fa5414b0c85e8a77d727e8ba2955748f379b
Tajinder S <63464204+Tejinder15@users.noreply.github.com>
2022-07-05 14:53:59 +0530

    refactor: added link to logo

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import { MdLogout } from "react-icons/md";
 import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 import { logoutUser } from "../../redux/features/auth/authSlice";
 const Header = () => {
   const dispatch = useDispatch();
@@ -9,10 +10,13 @@ const Header = () => {
   return (
     <header className="bg-white h-16 sticky top-0 shadow-md mb-4 z-50">
       <nav className="max-w-screen-xl mx-auto flex items-center justify-between h-full px-4">
-        <h1 className="text-3xl font-bold">Jinx</h1>
+        <Link to="/" className="text-3xl font-bold">
+          Jinx
+        </Link>
         <button
           className="text-2xl text-slate-500 hover:text-red-600"
           onClick={logoutHandler}
+          title="logout"
         >
           <MdLogout />
         </button>
